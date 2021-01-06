afd61eff8360998b384504c7f152948b8470c881
ttngu <tammyn137@gmail.com>
2021-01-06 15:01:32 -0800

    add schema and json return

--- models/workout.js
+++ models/workout.js
@@ -2,5 +2,47 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    exercises: [{
+        type: {
+            type: String,
+            trim: true,
+            required: true,
+            enum: ["cardio", "resistance"],
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        duration: {
+            type: Number,
+            min: 1,
+            required: true,
+        },
+        distance: {
+            type: Number,
+            min: 1,
+        },
+        weight: {
+            type: Number,
+            min: 1,
+        },
+        reps: {
+            type: Number,
+            min: 1,
+        },
+
+    }]
+}, 
+    // Return virtual fiels as JSON response
+    {
+    toJSON: {
+        virtuals: true
+    }
     
-})
+});
