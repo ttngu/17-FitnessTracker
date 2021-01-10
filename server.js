d46881fc5d7ba189858698fc45c3cc1e845deaf6
ttngu <tammyn137@gmail.com>
2021-01-09 19:20:26 -0800

    added required routes, still needs Mongo connection

--- server.js
+++ server.js
@@ -7,5 +7,17 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 // Express logging and routing
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+require("./routes/api-routes");
+require("./routes/html-routes");
 
 // Setup Mongo connection
+
+// App Listening
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+});
