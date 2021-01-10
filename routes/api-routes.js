3c24f9a27e5bb0d4dc01e1698915bbf32215a40f
ttngu <tammyn137@gmail.com>
2021-01-09 23:37:08 -0800

    updated pseudo code

---
+++ routes/api-routes.js
@@ -0,0 +1,10 @@
+// Import nosql model
+
+// Define API routes
+    // Get Latest workout
+
+    // Get All workouts 
+
+    // Insert new doc with current date as obejctID
+
+    // Update exercise subdoc array with new exercise
