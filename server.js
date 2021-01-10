abe64972568333ed3e527e7444ad935a63d5f21b
ttngu <tammyn137@gmail.com>
2021-01-09 19:00:43 -0800

    updated depedencies

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 
+const PORT = process.env.PORT || 8080;
+const app = express();
+
 // Express logging and routing
 
 // Setup Mongo connection
