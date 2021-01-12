87cc559072b550ca4018d4aacf21be1e8d1ebde2
ttngu <tammyn137@gmail.com>
2021-01-11 19:26:42 -0800

    updated routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,11 +12,35 @@ module.exports = (app) => {
                 res.json(err);
             });
     });
-
-}
-
     // Get All workouts 
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.find({}).then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+            .catch((err) => {
+                res.json(err);
+            });
+    });
 
     // Insert new doc with current date as obejctID
+    app.post("/api/workouts", (req, res) => {
+        db.Workout.create({}).then((dbWorkout) => {
+          // console.log(dbWorkout);
+          res.json(dbWorkout);
+        });
+    });
 
     // Update exercise subdoc array with new exercise
+    app.put("/api/workouts/:id", (req, res) => {
+        // console.log(req.params, req.body);
+        db.Workout.findByIdAndUpdate(req.params.id,
+          { $push: { exercises: req.body } }, { runValidators: true }).then((dbWorkout) => {
+          res.json(dbWorkout);
+        })
+          .catch((err) => {
+            res.json(err);
+          });
+    });
+};
+
+
