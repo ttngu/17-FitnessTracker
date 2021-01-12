8e7eb8c27655f1e0f6ec106402443ebddc18268e
ttngu <tammyn137@gmail.com>
2021-01-11 22:27:18 -0800

    updated mongodb connection, but still need help

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ require("./routes/api-routes");
 require("./routes/html-routes");
 
 // Setup Mongo connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://fitness-admin:<harrypotter>@cluster0.ftenn.mongodb.net/Workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+}).then(() => {
+  console.log("connected");
+});
+
 
 // App Listening
 app.listen(PORT, () => {
