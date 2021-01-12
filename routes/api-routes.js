e3b84d6dc12cb8b044437c11271b3cd4a4ff0a07
ttngu <tammyn137@gmail.com>
2021-01-11 22:41:30 -0800

    updated routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,46 +1,57 @@
 // Import nosql model
-const db = require("../models");
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
 
-// Define API routes
-module.exports = (app) => {
-    // Get Latest workout
-    app.get("/api/workouts", (req,res) => {
-        db.Workout.find({}).then((dbWorkout) => {
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+        .then(dbWorkout => {
             res.json(dbWorkout);
         })
-            .catch((err) => {
-                res.json(err);
-            });
-    });
-    // Get All workouts 
-    app.get("/api/workouts/range", (req, res) => {
-        db.Workout.find({}).then((dbWorkout) => {
-            res.json(dbWorkout);
-        })
-            .catch((err) => {
-                res.json(err);
-            });
-    });
-
-    // Insert new doc with current date as obejctID
-    app.post("/api/workouts", (req, res) => {
-        db.Workout.create({}).then((dbWorkout) => {
-          // console.log(dbWorkout);
-          res.json(dbWorkout);
-        });
-    });
-
-    // Update exercise subdoc array with new exercise
-    app.put("/api/workouts/:id", (req, res) => {
-        // console.log(req.params, req.body);
-        db.Workout.findByIdAndUpdate(req.params.id,
-          { $push: { exercises: req.body } }, { runValidators: true }).then((dbWorkout) => {
-          res.json(dbWorkout);
-        })
-          .catch((err) => {
+        .catch(err => {
             res.json(err);
-          });
-    });
-};
+        });
+});
 
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+            params.id, { $push: { exercises: body } }, { new: true, runValidators: true }
+        )
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
 
+router.get("/api/workouts", (req, res) => {
+    Workout.find()
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.get("/api/workouts/range", ({ query }, res) => {
+    Workout.find({ day: { $gte: query.start, $lte: query.end } })
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.delete("/api/workouts", ({ body }, res) => {
+    Workout.findByIdAndDelete(body.id)
+        .then(() => {
+            res.json(true);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+module.exports = router;
